816f1bc9f4e6f732beb9129c3a2742c8c0873f1b
2Flix <louisejourdan62@gmail.com>
2025-06-01 14:52:58 +0200

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<header>
-  <form action="upload_handler.php" method="POST" enctype="multipart/form-data">
-    <label style="cursor: pointer; font-weight: bold;">
-      Téléverser une image
-      <input type="file" name="image" onchange="this.form.submit()" hidden />
-    </label>
-  </form>
-</header>
