d9433906561f6732b57c8744d542f02f7e6b9962
Wartox99 <alexandre.steelandt@student.junia.com>
2025-06-12 16:18:22 +0200

    Add files via upload
    Modification du header pour mettre Scanalytix dans la barre

--- header.php
+++ header.php
@@ -1,10 +1,16 @@
 <header>
   <nav style="background-color:#333; padding:10px; display:flex; align-items:center; justify-content:space-between;">
-    <div>
-      <a href="visualiser.php" style="color:white; margin-right:20px; text-decoration:none; font-weight:bold;">Visualiser</a>
-      <a href="analyse.php" style="color:white; margin-right:20px; text-decoration:none; font-weight:bold;">Analyse</a>
+    <!-- Les 3 Liens à gauche -->
+    <div style="display: flex; gap: 20px;">
+      <a href="visualiser.php" style="color:white; text-decoration:none; font-weight:bold;">Visualiser</a>
+      <a href="analyse.php" style="color:white; text-decoration:none; font-weight:bold;">Analyse</a>
       <a href="traitement.php" style="color:white; text-decoration:none; font-weight:bold;">Traitement</a>
     </div>
+
+    <!-- Scanalytix au  centre -->
+    <div style="color:white; font-size: 20px; font-weight: bold;">Scanalytix</div>
+
+    <!-- Le Téléversement à droite -->
     <form action="upload_handler.php" method="POST" enctype="multipart/form-data" style="margin:0;">
       <label style="cursor: pointer; color:white; font-weight: bold;">
         Téléverser une image
@@ -13,4 +19,5 @@
     </form>
   </nav>
 </header>
+
 
