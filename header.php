1cf740e12b2c9a7d4abe3309e546bc004441af75
2Flix <louisejourdan62@gmail.com>
2025-06-15 11:08:31 +0200

    + Commentaires

--- header.php
+++ header.php
@@ -1,16 +1,14 @@
 <header>
   <nav style="background-color:#333; padding:10px; display:flex; align-items:center; justify-content:space-between;">
-    <!-- Les 3 Liens à gauche -->
+
     <div style="display: flex; gap: 20px;">
       <a href="visualiser.php" style="color:white; text-decoration:none; font-weight:bold;">Visualiser</a>
       <a href="analyse.php" style="color:white; text-decoration:none; font-weight:bold;">Analyse</a>
       <a href="traitement.php" style="color:white; text-decoration:none; font-weight:bold;">Traitement</a>
     </div>
 
-    <!-- Scanalytix au  centre -->
     <div style="color:white; font-size: 20px; font-weight: bold;">Scanalytix</div>
 
-    <!-- Le Téléversement à droite -->
     <form action="upload_handler.php" method="POST" enctype="multipart/form-data" style="margin:0;">
       <label style="cursor: pointer; color:white; font-weight: bold;">
         Téléverser une image
