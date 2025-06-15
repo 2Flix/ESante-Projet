5eb16e35d5d844681e1c03d62479dc7ff37efa77
2Flix <louisejourdan62@gmail.com>
2025-06-15 11:10:38 +0200

    + Commentaires

--- rotate_script.py
+++ rotate_script.py
@@ -12,22 +12,22 @@ def rotate_image(image_path, angle=90):
 
     rotated = ndimage.rotate(img_array, angle_for_scipy, reshape=True)
 
-    # Obtenir le répertoire, le nom de base du fichier et son extension
+    # Obtenir le repertoire, le nom de base du fichier et son extension
     directory, filename = os.path.split(image_path)
     base_name, extension = os.path.splitext(filename)
 
-    # Générer un timestamp unique
+    # Generer un timestamp unique
     timestamp = datetime.datetime.now().strftime("_%Y%m%d_%H%M%S")
 
     # Construire le nouveau nom de fichier
     new_filename = f"{base_name}{timestamp}{extension}"
     new_image_path = os.path.join(directory, new_filename)
 
-    # Enregistrer la nouvelle image rotatée
+    # Enregistrer la nouvelle image rotate
     iio.imwrite(new_image_path, rotated)
 
     # Retourner le chemin de la nouvelle image
-    print(new_image_path) # Afficher sur stdout pour que PHP puisse le récupérer
+    print(new_image_path) # Afficher sur stdout pour que PHP puisse le recuperer
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     image_path = sys.argv[1]
     # L'angle est maintenant toujours passé en argument 2
-    angle = float(sys.argv[2]) if len(sys.argv) > 2 else 90 # 90 par défaut si non fourni
+    angle = float(sys.argv[2]) if len(sys.argv) > 2 else 90 # 90 par defaut si non fourni
 
     if os.path.exists(image_path):
         rotate_image(image_path, angle)
